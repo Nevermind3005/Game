ab4761e6e405844825bafa08f4f8346d19a1a694
jakubelis <jakub.elis007@gmail.com>
2022-10-24 12:34:21 +0200

    Edited shooting

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,15 @@ public class Bullet : MonoBehaviour
 {
 
     [SerializeField] private float speed = 15f;
+    [SerializeField] private float destroyDistance = 10f;
+    private Transform firePointPosition;
 
     private Rigidbody2D rb;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        firePointPosition = GameObject.Find("FirePoint").GetComponent<Transform>();
     }
 
     void Start()
@@ -19,4 +23,11 @@ public class Bullet : MonoBehaviour
         rb.velocity = transform.right * speed;
     }
 
+    private void FixedUpdate()
+    {
+        if (Vector2.Distance(transform.position, firePointPosition.position) > destroyDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
